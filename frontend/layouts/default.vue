9d47663c6a047b4bd8601cb0fc737584dcc1933c
Vadim Ignatov <ignatov0131@gmail.com>
2022-10-16 19:08:52 +0300

    feat: add base layout

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <a-layout class="site-layout">
+    <a-layout-header>
+      Header
+    </a-layout-header>
+
+    <a-layout-content class="site-layout__content">
+      <Nuxt />
+    </a-layout-content>
+
+    <a-layout-footer>
+      Footer
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<script>
+export default {
+  name: 'Default'
+}
+</script>
+
+<style>
+  .site-layout {
+    display: flex;
+    min-height: 100vh;
+  }
+
+  .site-layout__content {
+    flex-grow: 1;
+  }
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial/>
+  <div>
+    main page
+  </div>
 </template>
 
 <script lang="ts">
