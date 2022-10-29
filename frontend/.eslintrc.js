7ff704bbbc640bb4ac49e3ea5f0a1a351fd88c97
Vadim Ignatov <ignatov0131@gmail.com>
2022-10-29 12:48:35 +0300

    feat: fix eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,10 +6,13 @@ module.exports = {
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
+    'plugin:nuxt/recommended'
   ],
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'vue/no-reserved-component-names': 'off',
+    'vue/multi-word-component-names': 'off'
+  }
 }
