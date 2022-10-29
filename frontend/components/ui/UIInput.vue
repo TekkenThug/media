4d95aa1449791162881328cbca61aaa875d4f168
Vadim Ignatov <ignatov0131@gmail.com>
2022-10-29 15:54:48 +0300

    feat: create ui input

---
+++ frontend/components/ui/UIInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="ui-input">
+    <input
+      :value="value"
+      type="text"
+      class="ui-input__field"
+      :placeholder="placeholder"
+      @change="$emit('change', $event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UIInput',
+
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.ui-input
+  border-radius: 9px
+  border: 2px solid #fff
+
+  &__field
+    width: 100%
+    padding: 12px 10px
+    outline: none
+    color: inherit
+
+    &::placeholder
+      color: #fff
+</style>
