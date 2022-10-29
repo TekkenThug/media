94a87ab25fe829f05a7264926d8652a1db6cc58a
Vadim Ignatov <ignatov0131@gmail.com>
2022-10-29 12:48:52 +0300

    feat: create header

---
+++ frontend/components/common/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header" />
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="sass" scoped>
+@import "assets/sass/_variables"
+
+.header
+  width: 100%
+  height: 60px
+  background-color: $slate-blue
+</style>
