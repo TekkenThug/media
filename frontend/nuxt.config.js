a929468f55fac380514f2f47f4834c2c3b41de76
Vadim Ignatov <ignatov0131@gmail.com>
2022-10-30 12:46:53 +0300

    feat: install vuelidate

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -34,7 +34,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/notify.ts',
-    '~/plugins/modal.ts'
+    '~/plugins/modal.ts',
+    '~/plugins/validate.ts'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ frontend/plugins/validate.ts
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+// @ts-ignore
+import Vuelidate from 'vuelidate'
+
+Vue.use(Vuelidate)
