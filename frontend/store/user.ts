1ee9fdc7aef5801278e20024af42b74f727e4fc0
Vadim Ignatov <ignatov0131@gmail.com>
2022-11-04 18:48:36 +0300

    feat: update user store

--- frontend/store/user.ts
+++ frontend/store/user.ts
@@ -1,9 +1,18 @@
+type Role = 'user' | 'editor' | 'admin' | 'copywriter' | ''
+
 interface State {
   isAuth: Boolean,
-  role: 'user' | 'editor' | 'admin' | 'copywriter' | ''
+  role: Role
 }
 
 export const state = (): State => ({
   isAuth: false,
   role: ''
 })
+
+export const mutations = {
+  setAuthState (state: State, role: Role) {
+    state.isAuth = true
+    state.role = role
+  }
+}
