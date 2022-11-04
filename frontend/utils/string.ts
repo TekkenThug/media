baf5378f08122c8468b0d7d3cd43bdb974f2feda
Vadim Ignatov <ignatov0131@gmail.com>
2022-11-04 23:17:47 +0300

    feat: add pluralize function

--- frontend/utils/string.ts
+++ frontend/utils/string.ts
@@ -1,3 +1,8 @@
 import { RUS_PHONE_REGEX } from '~/data/constats'
 
 export const isPhone = (value: string) => RUS_PHONE_REGEX.test(value)
+
+export const pluralize = (count: number, words: string[]) => {
+  const cases = [2, 0, 1, 1, 1, 2]
+  return count + ' ' + words[(count % 100 > 4 && count % 100 < 20) ? 2 : cases[Math.min(count % 10, 5)]]
+}
