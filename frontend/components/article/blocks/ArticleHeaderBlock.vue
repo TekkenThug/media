339e5814aa56286baa415d1c077d4114e74be7cd
Vadim Ignatov <ignatov0131@gmail.com>
2022-11-04 23:17:02 +0300

    feat: create first article blocks

---
+++ frontend/components/article/blocks/ArticleHeaderBlock.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :is="headLevel" class="article-block-header">
+    {{ text }}
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'ArticleHeaderBlock',
+
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+
+    level: {
+      type: [String, Number],
+      default: 2
+    }
+  },
+
+  computed: {
+    headLevel () {
+      return `h${this.level}`
+    }
+  }
+}
+</script>
---
+++ frontend/components/article/blocks/ArticleParagraphBlock.vue
@@ -0,0 +1,25 @@
+<template>
+  <p class="article-paragraph-block" v-html="text" />
+</template>
+
+<script>
+export default {
+  name: 'ArticleParagraphBlock',
+
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  @import 'assets/sass/mixins'
+
+  .article-paragraph-block
+    a
+      @include link
+
+</style>
