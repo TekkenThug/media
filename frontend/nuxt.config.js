2f00d7f02ad124c6ef8786a5a182b2a3377c56c0
Vadim Ignatov <ignatov0131@gmail.com>
2022-11-05 13:37:01 +0300

    Revert "feat: update progress bar color"
    This reverts commit bbbde3734a394dbcca37c1dc69023487e5eb93e6.

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -33,10 +33,6 @@ export default {
 
   pageTransition: 'fade',
 
-  loading: {
-    color: '#FC9E4F'
-  },
-
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/notify.ts',
