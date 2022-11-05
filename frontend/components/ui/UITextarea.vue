790ddaeeef2fc6ba0ff48b16c3e1e305e3d12848
Vadim Ignatov <ignatov0131@gmail.com>
2022-11-05 12:42:38 +0300

    feat: add slot for textarea

--- frontend/components/ui/UITextarea.vue
+++ frontend/components/ui/UITextarea.vue
@@ -1,10 +1,19 @@
 <template>
-  <textarea
-    class="ui-textarea"
-    :value="value"
-    :placeholder="placeholder"
-    @input="handleInput"
-  />
+  <div class="ui-textarea">
+    <textarea
+      :class="['ui-textarea__field', { '_pb': $slots.default }]"
+      :value="value"
+      :placeholder="placeholder"
+      :style="{
+        height: $slots.default ? '104px' : '64px'
+      }"
+      @input="handleInput"
+    />
+
+    <div v-if="$slots.default" class="ui-textarea__bottom">
+      <slot />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -31,7 +40,7 @@ export default {
     },
 
     changeHeight ({ target }) {
-      target.style.height = '64px'
+      target.style.height = this.$slots.default ? '104px' : '64px'
       target.style.height = (target.scrollHeight) + 'px'
     }
   }
@@ -42,19 +51,32 @@ export default {
   @import "assets/sass/variables"
 
   .ui-textarea
-    width: 100%
-    padding: 12px
-    resize: none
-    border-radius: 9px
-    outline: 1px solid $gainsboro
-    background-color: $cultured
-    // transition: height .2s linear
-    // outline: none
-    -webkit-box-shadow: none
-    -moz-box-shadow: none
-    box-shadow: none
-    transition: outline .2s ease
+    position: relative
 
-    &:focus
-      outline: 3px solid $slate-blue
+    &__field
+      width: 100%
+      padding: 12px
+      resize: none
+      border-radius: 9px
+      outline: 1px solid $gainsboro
+      background-color: $cultured
+      -webkit-box-shadow: none
+      -moz-box-shadow: none
+      box-shadow: none
+      transition: outline .2s ease
+
+      &._pb
+        padding-bottom: 60px
+
+      &:focus
+        outline: 3px solid $slate-blue
+
+    &__bottom
+      display: flex
+      flex-direction: row-reverse
+      position: absolute
+      bottom: 12px
+      left: 12px
+      right: 12px
+      height: 40px
 </style>
