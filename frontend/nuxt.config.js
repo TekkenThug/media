bbbde3734a394dbcca37c1dc69023487e5eb93e6
Vadim Ignatov <ignatov0131@gmail.com>
2022-11-05 13:36:50 +0300

    feat: update progress bar color

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -33,6 +33,10 @@ export default {
 
   pageTransition: 'fade',
 
+  loading: {
+    color: '#FC9E4F'
+  },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/notify.ts',
---
+++ frontend/pages/publications/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="publications">
+    <h1>Черновики</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PublicationsPage'
+}
+</script>
+
+<!--<style scoped>-->
+
+<!--</style>-->
