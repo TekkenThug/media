0564e019a596e6e55f20d06ccf6ae6ecff8505eb
Vadim Ignatov <ignatov0131@gmail.com>
2022-11-05 13:34:04 +0300

    feat: add page transition

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -31,6 +31,8 @@ export default {
     '~/assets/sass/_global.sass'
   ],
 
+  pageTransition: 'fade',
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/notify.ts',
