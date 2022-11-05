7012b5222f32a98a39c756ee956b9b917e8b2f27
Vadim Ignatov <ignatov0131@gmail.com>
2022-11-05 14:52:52 +0300

    feat: create modal article decline

---
+++ frontend/components/modals/modal-article-decline/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="modal-article-decline">
+    <h3 class="modal-article-decline__title">
+      Опишите причину отказа
+    </h3>
+
+    <UITextarea
+      v-model="reason"
+      placeholder="Причина"
+      class="modal-article-decline__textarea"
+    />
+
+    <v-btn
+      class="modal-article-decline__btn"
+      :disabled="!reason"
+      color="#6665DD"
+      @click="$emit('success')"
+    >
+      Отправить
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import UITextarea from '@/components/ui/UITextarea'
+
+export default {
+  name: 'ModalArticleDecline',
+
+  components: { UITextarea },
+
+  data () {
+    return {
+      reason: ''
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+  @import "assets/sass/variables"
+
+  .modal-article-decline
+    &__title
+      margin-bottom: 12px
+
+    &__textarea
+      margin-bottom: 12px
+
+    &__btn
+      margin-left: auto
+      color: $white
+</style>
